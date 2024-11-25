d6e324550117e90377f2c83c7735025f9bb2085e
Ayc023 <151034385+Ayc023@users.noreply.github.com>
2024-11-25 22:00:24 +0800

    Medicine Dispensed Modal Creation Done Backend Needed 👍

--- resources/views/livewire/pharmacy-table.blade.php
+++ resources/views/livewire/pharmacy-table.blade.php
@@ -338,19 +338,6 @@
                                 <option value="injectable">Injectable</option>
                             </select>
                         </div>
-                        <!-- Prescription Medicine -->
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="prescription_medicine"
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prescription
-                                Medicine</label>
-                            <select id="prescription_medicine" wire:model="prescription_medicine"
-                                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                <option value="" disabled selected>Is it Prescription Medicine?</option>
-                                <option value="yes">Yes</option>
-                                <option value="no">No</option>
-                            </select>
-                        </div>
-                    </div>
                     <div class="mt-4">
                         <button type="submit"
                             class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
